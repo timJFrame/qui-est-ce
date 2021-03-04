763f07b867e723ee6060275f416647a1edbba575
timframe <t.f.retouching@gmail.com>
2021-03-04 15:15:38 +0000

    secure route complete and added to post, put and delete routes

--- config/router.js
+++ config/router.js
@@ -1,18 +1,19 @@
 import express from 'express'
 import cards from '../controllers/cards.js'
 import auth from '../controllers/auth.js'
+import secureRoute from '../lib/secureRoute.js'
 
 const router = express.Router()
 
 router.route('/cards')
   .get(cards.index)
-  .post(cards.create)
+  .post(secureRoute, cards.create)
 
 
 router.route('/cards/:id')
-  .get(cards.show)
-  .put(cards.update)
-  .delete(cards.delete)
+  .get(secureRoute, cards.show)
+  .put(secureRoute, cards.update)
+  .delete(secureRoute, cards.delete)
 
 router.route('/register')
   .post(auth.registerUser)
